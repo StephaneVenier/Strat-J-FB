e9eb1a6a115aba3e95720f2028afd213c2e918c7
StephaneVenier <stephane.venier85@gmail.com>
2025-11-13 21:45:58 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// Remplis STRATJ_FIREBASE_CONFIG avec les infos de ton projet Firebase.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNgwNOuNOr9HQe7Z59IL_Zboc6CE2hJDs",
+  authDomain: "strat-j.firebaseapp.com",
+  projectId: "strat-j",
+  storageBucket: "strat-j-fb.vercel.app",
+  messagingSenderId: "332160517238",
+  appId: "1:332160517238:web:e12b2fdc98de65d9108b57",
+  measurementId: "G-JL8ZY7NCZ5"
+};
+window.STRATJ_FIREBASE_CONFIG = window.STRATJ_FIREBASE_CONFIG || {};
